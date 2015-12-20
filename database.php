be47466f95473cc82739a689d5495d3f1cfef886
Adil Mahmood <adil143m@gmail.com>
2015-12-20 21:34:11 +0000

    azure mail test8

--- database.php
+++ database.php
@@ -289,9 +289,40 @@ class Db {
     }//public function emailComfirmation($email) END
     public function emailSentToUser($email) {
        $emailToSent=$email; // Send email to our user
-       include "azureSentEmail.php";
+       //include "azureSentEmail.php";
         //$domainname= "http://bibliography.esy.es";
         //$domainname= "http://bibliography.azurewebsites.net";
+       require 'PHPMailer/PHPMailerAutoload.php';
+        $mail = new PHPMailer;
+
+        //$mail->SMTPDebug = 3;                          // Enable verbose debug output
+        $domainname= "http://bibliography.azurewebsites.net";
+        //$emailToSent='adil.mahmood.2012@mumail.ie';
+        $mail->isSMTP();                                        // Set mailer to use SMTP 
+        $mail->Host = 'smtp.sendgrid.net';             // Specify main/backup SMTP servers 
+        $mail->SMTPAuth = true;                           // Enable SMTP authentication 
+        $mail->Username = 'azure_cdf577451af1ffd1d93f2d69ce47caa2@azure.com';//'YOUR USERNAME';    // SMTP username 
+        $mail->Password = 'SLDLSLKsdsd45345';//'YOUR PASSWORD';    // SMTP password 
+        $mail->SMTPSecure = 'tls';                        // Enable TLS/SSL encryption 
+        $mail->Port = 587;                                      // TCP port to connect to
+
+        $mail->From = 'webmaster@bibliography.azurewebsites.net'; 
+        $mail->FromName = 'Bibliography Manager'; 
+        $mail->addAddress('adil143m@gmail.com', $emailToSent);     // Add a recipient
+        //$mail->CC('adil143m@gmail.com');     // Add a recipient
+
+        $mail->WordWrap = 50;                              // Set word wrap to 50 characters 
+        $mail->isHTML(true);                                  // Set email format to HTML
+
+        $mail->Subject = 'Email Verification'; 
+        $mail->Body    = '
+                            <p>Thanks for signing up!</p>
+                            <p>Your account has been created, you can login with the following credentials after you have activated</p>
+                            <p>your account by pressing the url below.</p>
+                            
+                            <b><p>Please click this link to activate your account:</p></b>
+                           
+                            '.$domainname.'/verify.php?email='.$emailToSent.'.';
         /********************************************************/
         // $to      = $email; // Send email to our user
         // $subject = 'Signup | Verification'; // Give the email a subject 
